fdc7200a79ac1c6ff6b6287bedfb864caa2854aa
rashadg1030 <rashad.sasaki@gmail.com>
2018-12-25 13:02:07 -0800

    Textarea is translating romaji to hiragana properly.

--- components/Textarea.js
+++ components/Textarea.js
@@ -1,11 +1,12 @@
 m = require("mithril");
 translate = require("../js/translate");
 
+function sayHi() {
+    alert("hello");
+}
+
 module.exports = {
     view: function(vnode) {
-        return m("textarea", {placeholder: "Type here"})
-    },
-    onupdate: function(vnode) {
-        alert(this.text);
+        return m("textarea", {placeholder: "Type here", onkeyup: function() {vnode.dom.value = translate(vnode.dom.value)}})
     }
 }
